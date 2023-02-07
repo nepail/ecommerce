b4e6bb1ae0a6bc3d2a52211c8297e54b03935fcc
nepail <kls18z@hotmail.com>
2023-02-07 17:38:43 +0800

    add StateContext

--- ecommerce/src/pages/_app.js
+++ ecommerce/src/pages/_app.js
@@ -1,11 +1,15 @@
-import '@/styles/globals.css'
-import { Layout } from '../components'
-
+import "@/styles/globals.css";
+import { Layout } from "../components";
+import { StateContext } from "../context/StateContext";
+import {Toaster} from 'react-hot-toast'
 
 export default function App({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
-  )
+    <StateContext>
+      <Layout>
+        <Toaster />
+        <Component {...pageProps} />
+      </Layout>
+    </StateContext>
+  );
 }
--- ecommerce/src/pages/product/[slug].js
+++ ecommerce/src/pages/product/[slug].js
@@ -8,10 +8,12 @@ import {
 } from "react-icons/ai";
 
 import { Product } from "../../components";
+import {useStateContext} from '../../context/StateContext'
 
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
+  const {decQty, incQty, qty, onAdd} = useStateContext()
 
   return (
     <div>
@@ -47,19 +49,19 @@ const ProductDetails = ({ product, products }) => {
           <div className="quantity">
             <h3>Quantity:</h3>
             <p className="quantity-desc">
-              <span className="minus" onClick="">
+              <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
               <span className="num" onClick="">
-                0
+                {qty}
               </span>
-              <span className="plus" onClick="">
+              <span className="plus" onClick={incQty}>
                 <AiOutlinePlus />
               </span>
             </p>
           </div>
           <div className="buttons">
-            <button type="button" className="add-to-cart" onClick="">
+            <button type="button" className="add-to-cart" onClick={()=>onAdd(product, qty)}>
               Add to Cart
             </button>
             <button type="button" className="buy-now" onClick="">
