2296653721d36915585d52992687a187fa927b6e
nepail <kls18z@hotmail.com>
2023-02-08 11:49:25 +0800

    add shipping rate

--- ecommerce/src/components/Cart.jsx
+++ ecommerce/src/components/Cart.jsx
@@ -7,15 +7,47 @@ import {
   AiOutlineShopping,
 } from "react-icons/ai";
 import { TiDeleteOutline } from "react-icons/ti";
-import totast from "react-hot-toast";
+import toast from "react-hot-toast";
 
 import { useStateContext } from "../context/StateContext";
 import { urlFor } from "../lib/client";
+import getStripe from '../lib/getStripe'
 
 const Cart = () => {
   const cartRef = useRef();
-  const { totalPrice, totalQuantities, cartItems, setShowCart, toggleCartItemQuanitity, onRemove } =
-    useStateContext();
+  const {
+    totalPrice,
+    totalQuantities,
+    cartItems,
+    setShowCart,
+    toggleCartItemQuanitity,
+    onRemove,
+  } = useStateContext();
+
+  const handleCheckout = async () => {
+    const stripe = await getStripe()
+
+    console.log(cartItems)
+
+    const response = await fetch('/api/stripe', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',        
+      },
+      body: JSON.stringify(cartItems)
+    })
+
+    console.log('response')
+    console.log(response)
+
+    if(response.statusCode === 500) return
+    const data = await response.json()
+    toast.loading('Redirecting...')
+    console.log(stripe)
+    console.log(data)
+    stripe.redirectToCheckout({ sessionId: data.id });
+  }
+
   return (
     <div className="cart-wrapper" ref={cartRef}>
       <div className="cart-container">
@@ -61,18 +93,32 @@ const Cart = () => {
                   <div className="flex bottom">
                     <div>
                       <p className="quantity-desc">
-                        <span className="minus" onClick={()=>toggleCartItemQuanitity(item._id, 'dec')}>
+                        <span
+                          className="minus"
+                          onClick={() =>
+                            toggleCartItemQuanitity(item._id, "dec")
+                          }
+                        >
                           <AiOutlineMinus />
                         </span>
                         <span className="num" onClick="">
                           {item.quantity}
                         </span>
-                        <span className="plus" onClick={()=>toggleCartItemQuanitity(item._id, 'inc')}>
+                        <span
+                          className="plus"
+                          onClick={() =>
+                            toggleCartItemQuanitity(item._id, "inc")
+                          }
+                        >
                           <AiOutlinePlus />
                         </span>
                       </p>
                     </div>
-                    <button type="button" className="remove-item" onClick={()=>onRemove(item)}>
+                    <button
+                      type="button"
+                      className="remove-item"
+                      onClick={() => onRemove(item)}
+                    >
                       <TiDeleteOutline />
                     </button>
                   </div>
@@ -87,7 +133,7 @@ const Cart = () => {
               <h3>${totalPrice}</h3>
             </div>
             <div className="btn-container">
-              <button type="button" className="btn" onClick="">
+              <button type="button" className="btn" onClick={handleCheckout}>
                 Pay with Stripe
               </button>
             </div>
--- ecommerce/src/pages/product/[slug].js
+++ ecommerce/src/pages/product/[slug].js
@@ -27,7 +27,7 @@ const ProductDetails = ({ product, products }) => {
           </div>
           <div className="small-images-container">
             {image?.map((item, i) => (
-              <img src={urlFor(item)} className={i === index ? 'small-image selected-image' : 'small-image'} onMouseEnter={()=>setIndex(i)} />
+              <img key={i} src={urlFor(item)} className={i === index ? 'small-image selected-image' : 'small-image'} onMouseEnter={()=>setIndex(i)} />
             ))}
           </div>
         </div>
