cf1ad4f6519fa08b5467f72a9e1574d31ad47fde
nepail <kls18z@hotmail.com>
2023-02-08 12:27:43 +0800

    update success page

--- ecommerce/src/pages/product/[slug].js
+++ ecommerce/src/pages/product/[slug].js
@@ -13,7 +13,13 @@ import {useStateContext} from '../../context/StateContext'
 const ProductDetails = ({ product, products }) => {
   const { image, name, details, price } = product;
   const [index, setIndex] = useState(0);
-  const {decQty, incQty, qty, onAdd} = useStateContext()
+  const {decQty, incQty, qty, onAdd, setShowCart} = useStateContext()
+
+  const handleBuyNow = () => {
+    onAdd(product, qty)
+
+    setShowCart(true)
+  }
 
   return (
     <div>
@@ -52,7 +58,7 @@ const ProductDetails = ({ product, products }) => {
               <span className="minus" onClick={decQty}>
                 <AiOutlineMinus />
               </span>
-              <span className="num" onClick="">
+              <span className="num">
                 {qty}
               </span>
               <span className="plus" onClick={incQty}>
@@ -64,7 +70,7 @@ const ProductDetails = ({ product, products }) => {
             <button type="button" className="add-to-cart" onClick={()=>onAdd(product, qty)}>
               Add to Cart
             </button>
-            <button type="button" className="buy-now" onClick="">
+            <button type="button" className="buy-now" onClick={handleBuyNow}>
               Buy Now
             </button>
           </div>
